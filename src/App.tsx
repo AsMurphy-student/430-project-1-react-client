48bab0855f0a7d6c1dc25855f72dead2f8342dfb
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-10-05 12:23:26 -0400

    Created get all books by title handler and tested build

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,27 @@ import './App.css'
 
 function App() {
   const [getAllBooks, setGetAllBooks] = useState(false);
+  const [getAllBooksByTitle, setGetAllBooksByTitle] = useState(false);
 
   const [getAllBooksMethod, setGetAllBooksMethod] = useState('GET');
+  const [getAllBooksByTitleMethod, setGetAllBooksByTitleMethod] = useState('GET');
+
+  const [getAllBooksByTitleOption, setGetAllBooksByTitleOption] = useState('');
 
   const [getAllBooksOutput, setGetAllBooksOutput] = useState('');
+  const [getAllBooksByTitleOutput, setGetAllBooksByTitleOutput] = useState('');
 
-  const radioHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const getAllBooksHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setGetAllBooksMethod(event.target.value);
   };
+
+  const getAllBooksByTitleHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGetAllBooksByTitleMethod(event.target.value);
+  };
+
+  const getAllBooksByTitleTitleHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGetAllBooksByTitleOption(event.target.value);
+  }
 
   return (
     <div className='bg-black min-h-screen'>
@@ -28,7 +41,7 @@ function App() {
                 value="GET"
                 id='get'
                 checked={getAllBooksMethod === 'GET'}
-                onChange={radioHandler}
+                onChange={getAllBooksHandler}
               />
               <label>GET</label>
             </p>
@@ -40,7 +53,7 @@ function App() {
                 value="HEAD"
                 id='head'
                 checked={getAllBooksMethod === 'HEAD'}
-                onChange={radioHandler}
+                onChange={getAllBooksHandler}
               />
               <label>HEAD</label>
             </p>
@@ -52,8 +65,6 @@ function App() {
                   'Accept': 'application/json',
                 },
               });
-
-              console.log(response);
 
               if (!response.ok) {
                 console.log('error');
@@ -81,6 +92,80 @@ function App() {
               Working?
             </button>
           <p className='text-white whitespace-pre-wrap'>{getAllBooksOutput}</p>
+        </div>
+      }
+
+      <button className='text-white' onClick={() => setGetAllBooksByTitle(!getAllBooksByTitle)}>Get All Books By Title</button>
+      {
+        getAllBooksByTitle && 
+        <div>
+          <fieldset>
+            <p className='text-white'>
+              <input
+                type="radio"
+                name='method'
+                value="GET"
+                id='get'
+                checked={getAllBooksByTitleMethod === 'GET'}
+                onChange={getAllBooksByTitleHandler}
+              />
+              <label>GET</label>
+            </p>
+
+            <p className='text-white'>
+              <input
+                type="radio"
+                name='method'
+                value="HEAD"
+                id='head'
+                checked={getAllBooksByTitleMethod === 'HEAD'}
+                onChange={getAllBooksByTitleHandler}
+              />
+              <label>HEAD</label>
+            </p>
+            <p className='text-white'>
+              <input
+                name='title'
+                id='title'
+                onChange={getAllBooksByTitleTitleHandler}
+              />
+              <label>Title: </label>
+            </p>
+          </fieldset>
+          <button className='text-white' onClick={async () => {
+              const response = await fetch(`/getBooksByTitle?title=${getAllBooksByTitleOption}`, {
+                method: getAllBooksByTitleMethod,
+                headers: {
+                  'Accept': 'application/json',
+                },
+              });
+
+              if (!response.ok) {
+                console.log('error');
+              }
+              else if (response.body) {
+                const jsonData = await response.json();
+
+                const output = `
+                Status: ${response.status}\n
+                Content-Length: ${response.headers.get('content-length')}
+                Response: [${JSON.stringify(jsonData)}]
+                `;
+
+                setGetAllBooksByTitleOutput(output);
+              }
+              else{
+                const output = `
+                Status: ${response.status}\n
+                Content-Length: ${response.headers.get('content-length')}
+                `;
+
+                setGetAllBooksByTitleOutput(output);
+              }
+            }}>
+              Working?
+            </button>
+          <p className='text-white whitespace-pre-wrap'>{getAllBooksByTitleOutput}</p>
         </div>
       }
     </div>
