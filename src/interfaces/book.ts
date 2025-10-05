35202266bd270db5675dda42b97ffa1cb62c268b
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-10-05 10:06:16 -0400

    Got build working on backend repo

---
+++ src/interfaces/book.ts
@@ -0,0 +1,10 @@
+export interface Book {
+    author: string;
+    country: string;
+    language: string;
+    link: string;
+    pages: string;
+    title: string;
+    year: string;
+    genres?: string[];
+}
