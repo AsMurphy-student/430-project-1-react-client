9b5d562456db7c1c95b152262a56bbbbf0e11d82
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-10-05 19:18:35 -0400

    Created helper component for get requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,108 @@
 import { useState } from 'react';
 import './App.css'
 
+function GetModule(props: { 
+    moduleState: boolean, 
+    moduleStateHandler: React.Dispatch<React.SetStateAction<boolean>>,
+    methodType: string,
+    methodTypeHandler: React.Dispatch<React.SetStateAction<string>>,
+    searchTerm: string,
+    searchTermHandler: React.Dispatch<React.SetStateAction<string>>,
+    outputResult: string,
+    outputResultHandler: React.Dispatch<React.SetStateAction<string>>,
+    queryType: string,
+  }) {
+
+  const { moduleState, moduleStateHandler, methodType, methodTypeHandler, searchTerm, searchTermHandler, outputResult, outputResultHandler, queryType } = props;
+
+  const searchTermOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    searchTermHandler(event.target.value);
+  }
+
+  const methodTypeOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    methodTypeHandler(event.target.value);
+  };
+
+  return (
+    <>
+      <button className='text-white' onClick={() => moduleStateHandler(!moduleState)}>Get All Books By {queryType}</button>
+      <hr />
+      {
+        moduleState && 
+        <div>
+          <fieldset>
+            <p className='text-white'>
+              <input
+                type="radio"
+                name='method'
+                value="GET"
+                id='get'
+                checked={methodType === 'GET'}
+                onChange={methodTypeOnChange}
+              />
+              <label>GET</label>
+            </p>
+
+            <p className='text-white'>
+              <input
+                type="radio"
+                name='method'
+                value="HEAD"
+                id='head'
+                checked={methodType === 'HEAD'}
+                onChange={methodTypeOnChange}
+              />
+              <label>HEAD</label>
+            </p>
+            <p className='text-white'>
+              <input
+                name='title'
+                id='title'
+                onChange={searchTermOnChange}
+              />
+              <label>{queryType}: </label>
+            </p>
+          </fieldset>
+          <button className='text-white' onClick={async () => {
+              const response = await fetch(`/getBooksByTitle?${queryType}=${searchTerm}`, {
+                method: methodType,
+                headers: {
+                  'Accept': 'application/json',
+                },
+              });
+
+              if (!response.ok) {
+                console.log('error');
+              }
+              else if (response.body) {
+                const jsonData = await response.json();
+
+                const output = `
+                Status: ${response.status}\n
+                Content-Length: ${response.headers.get('content-length')}
+                Response: [${JSON.stringify(jsonData)}]
+                `;
+
+                outputResultHandler(output);
+              }
+              else{
+                const output = `
+                Status: ${response.status}\n
+                Content-Length: ${response.headers.get('content-length')}
+                `;
+
+                outputResultHandler(output);
+              }
+            }}>
+              Working?
+            </button>
+          <p className='text-white whitespace-pre-wrap'>{outputResult}</p>
+        </div>
+      }
+    </>
+  );
+}
+
 function App() {
   const [getAllBooks, setGetAllBooks] = useState(false);
   const [getAllBooksByTitle, setGetAllBooksByTitle] = useState(false);
@@ -17,19 +119,20 @@ function App() {
     setGetAllBooksMethod(event.target.value);
   };
 
-  const getAllBooksByTitleHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setGetAllBooksByTitleMethod(event.target.value);
-  };
+  // const getAllBooksByTitleHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setGetAllBooksByTitleMethod(event.target.value);
+  // };
 
-  const getAllBooksByTitleTitleHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setGetAllBooksByTitleOption(event.target.value);
-  }
+  // const getAllBooksByTitleTitleHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setGetAllBooksByTitleOption(event.target.value);
+  // }
 
   return (
     <div className='bg-black min-h-screen'>
       <h1 className='text-white'>Project 1 Book API</h1>
 
       <button className='text-white' onClick={() => setGetAllBooks(!getAllBooks)}>Get All Books</button>
+      <br />
       {
         getAllBooks && 
         <div>
@@ -95,7 +198,8 @@ function App() {
         </div>
       }
 
-      <button className='text-white' onClick={() => setGetAllBooksByTitle(!getAllBooksByTitle)}>Get All Books By Title</button>
+      {/* <button className='text-white' onClick={() => setGetAllBooksByTitle(!getAllBooksByTitle)}>Get All Books By Title</button>
+      <br />
       {
         getAllBooksByTitle && 
         <div>
@@ -167,7 +271,17 @@ function App() {
             </button>
           <p className='text-white whitespace-pre-wrap'>{getAllBooksByTitleOutput}</p>
         </div>
-      }
+      } */}
+      <GetModule 
+      moduleState={getAllBooksByTitle} 
+      moduleStateHandler={setGetAllBooksByTitle} 
+      methodType={getAllBooksByTitleMethod} 
+      methodTypeHandler={setGetAllBooksByTitleMethod} 
+      searchTerm={getAllBooksByTitleOption} 
+      searchTermHandler={setGetAllBooksByTitleOption} 
+      outputResult={getAllBooksByTitleOutput} 
+      outputResultHandler={setGetAllBooksByTitleOutput} 
+      queryType={'title'} />
     </div>
   )
 }
