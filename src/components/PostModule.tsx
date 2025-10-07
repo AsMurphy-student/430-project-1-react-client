5e64a678513fdcfcaf7da3ab847e981ab9b0a8fd
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-10-07 13:30:53 -0400

    Created and set up states for input fields for post requests

--- src/components/PostModule.tsx
+++ src/components/PostModule.tsx
@@ -3,42 +3,90 @@
 import { useState } from "react";
 import type { BookParams } from "../interfaces/BookParams";
 
-function AddBookInput(props: { query: string, bookObj: BookParams }) {
-  const { query, bookObj } = props;
+function AddBookInput(props: { query: string, bookObj: BookParams, setBookObj: React.Dispatch<React.SetStateAction<BookParams>> }) {
+  const { query, bookObj, setBookObj } = props;
+
+  const renderValue = (value: string) => {
+    switch (value) {
+      case 'author':
+        return bookObj.author;
+      case 'country':
+        return bookObj.country;
+      case 'language':
+        return bookObj.language;
+      case 'link':
+        return bookObj.link;
+      case 'pages':
+        return bookObj.pages;
+      case 'title':
+        return bookObj.title;
+      case 'year':
+        return bookObj.year;
+      case 'genres':
+        return bookObj.genres?.join(',');
+    }
+  }
   return (
     <p className='text-white'>
-      <label>{query}: </label>
+      <label>{query === 'genre' ? 'Genre (use , as separator)' : query}: </label>
       <input
         name={`${query}-term`}
         id='term'
-        value={bookObj.author}
+        value={renderValue(query)}
+        // inputMode={query === 'pages' || query === 'year' ? 'numeric' : 'search'}
         onChange={(event: React.ChangeEvent<HTMLInputElement>) => 
           {
             switch (query) {
               case 'author':
-                bookObj.author = event.target.value
+                setBookObj({
+                  ...bookObj,
+                  author: event.target.value,
+                });
                 break;
               case 'country':
-                bookObj.country = event.target.value
+                setBookObj({
+                  ...bookObj,
+                  country: event.target.value,
+                });
                 break;
               case 'language':
-                bookObj.language = event.target.value
+                setBookObj({
+                  ...bookObj,
+                  language: event.target.value,
+                });
                 break;
               case 'link':
-                bookObj.link = event.target.value
+                setBookObj({
+                  ...bookObj,
+                  link: event.target.value,
+                });
                 break;
               case 'pages':
-                bookObj.pages = Number(event.target.value)
-                break;
+                if (!Number.isNaN(Number(event.target.value)))
+                  setBookObj({
+                    ...bookObj,
+                    pages: Number(event.target.value),
+                  });
+                  break;
               case 'title':
-                bookObj.title = event.target.value
+                setBookObj({
+                  ...bookObj,
+                  title: event.target.value,
+                });
                 break;
               case 'year':
-                bookObj.year = Number(event.target.value)
+                if (!Number.isNaN(Number(event.target.value)))
+                  setBookObj({
+                    ...bookObj,
+                    year: Number(event.target.value),
+                  });
+                  break;
+              case 'genres':
+                setBookObj({
+                  ...bookObj,
+                  genres: event.target.value.split(','),
+                });
                 break;
-              // case 'genres':
-              //   bookObj.author = event.target.value
-              //   break;
             }
           }}
       />
@@ -52,7 +100,7 @@ function PostModule(props: { queryURL: string, }) {
   const [moduleState, setModuleState] = useState(false);
   const [outputResult, setOutputResult] = useState('');
 
-  const book: BookParams = {
+  const [newBook, setNewBook] = useState<BookParams>({
     author: "",
     country: "",
     language: "",
@@ -60,7 +108,11 @@ function PostModule(props: { queryURL: string, }) {
     pages: 0,
     title: "",
     year: 0,
-  }
+  });
+
+  const [title, setTitle] = useState('');
+  const [genre, setGenre] = useState('');
+
   return (
     <>
       <button className='text-white' onClick={() => {
@@ -75,32 +127,45 @@ function PostModule(props: { queryURL: string, }) {
             {
               queryURL === "/addBook" &&
               <>
-                <AddBookInput query={"author"} bookObj={book} />
-                <AddBookInput query={"country"} bookObj={book} />
-                <AddBookInput query={"language"} bookObj={book} />
-                <AddBookInput query={"link"} bookObj={book} />
-                <AddBookInput query={"pages"} bookObj={book} />
-                <AddBookInput query={"title"} bookObj={book} />
-                <AddBookInput query={"year"} bookObj={book} />
+                <AddBookInput query={"author"} bookObj={newBook} setBookObj={setNewBook} />
+                <AddBookInput query={"country"} bookObj={newBook} setBookObj={setNewBook} />
+                <AddBookInput query={"language"} bookObj={newBook} setBookObj={setNewBook} />
+                <AddBookInput query={"link"} bookObj={newBook} setBookObj={setNewBook} />
+                <AddBookInput query={"pages"} bookObj={newBook} setBookObj={setNewBook} />
+                <AddBookInput query={"title"} bookObj={newBook} setBookObj={setNewBook} />
+                <AddBookInput query={"year"} bookObj={newBook} setBookObj={setNewBook} />
+                <AddBookInput query={"genre"} bookObj={newBook} setBookObj={setNewBook} />
               </>
             }
             {
               queryURL === "/addGenre" &&
-              <p>add genre here</p>
+              <>
+                <p className='text-white'>
+                  <label>title: </label>
+                  <input
+                    name={`title-genre-term`}
+                    id='term'
+                    value={title}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => 
+                      setTitle(event.target.value)}
+                  />
+                </p>
+                <p className='text-white'>
+                  <label>genre (just one genre): </label>
+                  <input
+                    name={`genre-genre-term`}
+                    id='term'
+                    value={genre}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => 
+                      setGenre(event.target.value)}
+                  />
+                </p>
+              </>
             }
           </fieldset>
           <button className='text-white' onClick={async () => {
             if (queryURL === '/addBook') {
-              setOutputResult('');
-              // const bookToAdd : BookParams = {
-              //   author: "",
-              //   country: "",
-              //   language: "",
-              //   link: "",
-              //   pages: 0,
-              //   title: "",
-              //   year: 0
-              // }
+              
             }
             else {
 
